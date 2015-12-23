a51d5db3d2e0e4da460f682f810b741da61292b5
mattesja <mm4242@googlemail.com>
2015-12-23 12:41:32 +0100

    dynamic quiz no update

--- src/state/actions/quiz.js
+++ src/state/actions/quiz.js
@@ -4,16 +4,19 @@ import { playSound } from 'utils/sound';
 export const type = 'SOLVE';
 
 export function reduce(state, { key }) {
-  console.log("reduce" + Object.keys(state));
+  return flow(answerQuiz)(state, key);
+}
+
+export function answerQuiz(state, key) {
   const quiz = state.get('quiz');
   const entity = state.get('entity');
   if (entity && entity.get('type') === 'houseA')
     if (quiz && quiz.get('solution') === key) {
       console.log('solved');
       playSound('oh');
-      return state.update('health', (h) => h + 1);
-  }
-  else {
+      return flow(incrementHealth, askNewQuiz)(state);
+    }
+    else {
       console.log('wrong');
       playSound('hit-shriek');
       return state.update('health', (h) => h - 1);
@@ -21,14 +24,43 @@ export function reduce(state, { key }) {
   return state;
 }
 
+function incrementHealth(state) {
+  return state.update('health', (h) => h + 1);
+}
+
+function askNewQuiz(state) {
+  return state.setIn('quiz', askQuiz());
+}
+
 export function solveQuiz(key) {
   return { type, key };
 };
 
 export function askQuiz() {
-  console.log('askQuiz()');
+
+  const firstNumber = getRandom(19, 5);
+  const secondNumber = getRandom(20 - firstNumber, 1);
+  var solution = firstNumber + secondNumber;
+  var question = firstNumber + ' + ' + secondNumber + ' = ?';
+
+  console.log('askQuiz()' + firstNumber + " " + secondNumber + " " + solution);
+
+  if (secondNumber < 10) {
+    question = solution + ' - ' + firstNumber + ' = ?';
+    solution = secondNumber;
+  } else if (solution > 9) {
+    solution = Math.abs(firstNumber - secondNumber);
+    question = secondNumber + ' + ? = ' + firstNumber;
+  }
+
+  console.log('askQuiz() ' + question + "==" + solution + " "+ firstNumber + " " + secondNumber + " " );
+
   return {
-    question: '1 + 2 = ?',
-    solution: '3'
+    question: question,
+    solution: solution + ''
   };
+}
+
+export function getRandom(maximum, minimum) {
+  return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 }
