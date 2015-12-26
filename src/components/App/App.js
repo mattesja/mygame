499d3f430cedd4ae7742d93c29988b50eb125907
mattesja <mm4242@googlemail.com>
2015-12-26 20:30:06 +0100

    Revert "kein editor"
    This reverts commit 6755ed8ab0d4b559650a14f79984f4e45e48090f.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,6 +20,18 @@ export default createPureComponent({
     return (
       <div className="app">
         {this.props.children}
+        <nav className="navbar">
+          <div className="navbar__item">
+            {this.renderLink()}
+          </div>
+          <div className="navbar__item">
+            <iframe
+              src="https://ghbtns.com/github-btn.html?user=stevenhauser&repo=i-have-to-return-some-videotapes&type=star"
+              className="navbar__btn"
+              frameBorder="0" scrolling="0" width="50px" height="20px"
+            />
+          </div>
+        </nav>
       </div>
     );
   }
